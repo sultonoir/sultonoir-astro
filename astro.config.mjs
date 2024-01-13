9e1086126da2e54d3d5b75f7a88ef78a45b3062b
Sulton <sultonalidrus5@gmail.com>
2024-01-13 17:06:03 +0700

    ref : change to vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,12 @@
 import { defineConfig } from "astro/config";
 import vercel from "@astrojs/vercel/serverless";
 import react from "@astrojs/react";
-import vercelServerless from "@astrojs/vercel/serverless";
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   output: "hybrid",
-  adapter: vercelServerless(),
+  adapter: vercel(),
   integrations: [
     react(),
     tailwind({
